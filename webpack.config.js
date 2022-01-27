ecc13d54d8336756361f40bb5463b5f56e90c48e
Juanitoclement <xdigoxinx@gmail.com>
2022-01-28 01:43:13 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    // Where files should be sent once they are bundled
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    // Rules of how webpack will take our files, complie & bundle them for the browser
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({template: './public/index.html'})]
+}
